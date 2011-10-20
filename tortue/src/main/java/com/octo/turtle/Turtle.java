07a2c2443598d467bbf6d941a11eedd19f61ace0
Mathieu Gandin <mga@octo.com>
2011-10-20 16:28:55 +0200

    Gymnastique meta-programmation en java

--- tortue/src/main/java/com/octo/turtle/Turtle.java
+++ tortue/src/main/java/com/octo/turtle/Turtle.java
@@ -1,12 +1,22 @@
 package com.octo.turtle;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Turtle {
 
 	public static void main(String[] args) throws IOException {
-		Interpreter interpreter = new Interpreter(new Instructions(new Instruction()));
-		System.out.println(interpreter.execute(new Command(args[0])));
+		
+		
+		while(true) {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			Interpreter interpreter = new Interpreter(new Instructions(new Instruction()));
+			String commandAsString = reader.readLine();
+			if(commandAsString.equals("q"))
+				System.exit(0);
+			System.out.println(interpreter.execute(new Command(commandAsString)));
+		}
 	}
 
 }
